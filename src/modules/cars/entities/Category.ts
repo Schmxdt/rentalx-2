d259d3a2832c7740df34bf5d841225fac9920e4c
root <root@DESKTOP-L4AOKGT>
2023-01-17 09:40:07 -0300

    aula 8

---
+++ src/modules/cars/entities/Category.ts
@@ -0,0 +1,28 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn } from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+/* ? siginifica opcional */
+@Entity("categories")
+class Category {
+  @PrimaryColumn()
+  id?: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  /* metodo constructor e chamado quando a clase eh instnaciada */
+  constructor() {
+    if (!this.id) {
+      // se nao tiver nenhum id
+      this.id = uuidV4();
+    } // id recebe um valor do UUIDV4
+  }
+}
+
+export { Category };
