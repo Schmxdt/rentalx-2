012adc9473aa28ee97d04bc39ed465545c86d8e4
Schmxdt <airtonschmidt2018@gmail.com>
2023-01-25 13:59:52 -0300

    rentals created

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,25 +1,36 @@
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+
+import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
 import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/CategoriesRepository";
-import { SpecificationRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationRepository";
+
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
-import { ISpecificationRepository } from "@modules/cars/repositories/ISpecificationRepository";
+
+
+import { SpecificationsRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationRepository";
+import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationRepository";
+
+
+import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
+import { CarsImagesRepository } from "@modules/cars/infra/typeorm/repositories/CarsImagesRepository";
+
 import { container } from "tsyringe";
-
-
 
 // ICategoriesRepository
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
   CategoriesRepository
 );
-
-container.registerSingleton<ISpecificationRepository>(
-  "SpecificationRepository",
-  SpecificationRepository
+container.registerSingleton<ISpecificationsRepository>(
+  "SpecificationsRepository",
+  SpecificationsRepository
 );
-
 container.registerSingleton<IUsersRepository>(
   "UsersRepository",
   UsersRepository
 );
+container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
+
+container.registerSingleton<ICarsImagesRepository>("CarsImagesRepository", CarsImagesRepository);
+
--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,13 +1,35 @@
 import { Router } from "express";
 
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
+import { ListAvailableCarsController } from "@modules/cars/useCases/ListAvailableCars/ListAvailableCarsController";
+
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
+import { UploadCarImagesController } from "@modules/cars/useCases/uploadCarImage/UploadCarImagesController";
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
+const listAvailableCarsController = new ListAvailableCarsController();
+const createCarSpecificationController = new CreateCarSpecificationController();
+const uploadCarImagesController = new UploadCarImagesController();
 
-carsRoutes.post("/", ensureAuthenticated, ensureAdmin, createCarController.handle)
+
+carsRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createCarController.handle
+);
+
+carsRoutes.get("/available", listAvailableCarsController.handle);
+
+carsRoutes.post(
+  "/specifications/:id", ensureAuthenticated, ensureAdmin, createCarSpecificationController.handle
+);
+
+carsRoutes.post("/images/:id", ensureAuthenticated,
+  ensureAdmin, uploadCarImagesController.handle);
 
 export { carsRoutes }
