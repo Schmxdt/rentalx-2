dd60bbbae758ea26deecd1fa88112f5231912540
Schmxdt <airtonschmidt2018@gmail.com>
2023-01-31 21:41:29 -0300

    Created Owner Routes

--- src/shared/infra/http/routes/owner.routes.ts
+++ src/shared/infra/http/routes/owner.routes.ts
@@ -1,10 +1,11 @@
 import { CreateOwnerController } from "@modules/owner/useCases/createOwner/CreateOwnerController";
+import { DeleteOwnerController } from "@modules/owner/useCases/deleteOwner/DeleteOwnerController";
 import { Router } from "express";
 
 const ownerRoutes = Router()
 
-const createOwnerController = new CreateOwnerController();
+ownerRoutes.post('/', new CreateOwnerController().handle)
 
-ownerRoutes.post('/', createOwnerController.handle)
+ownerRoutes.delete('/:id', new DeleteOwnerController().handle)
 
 export { ownerRoutes }
