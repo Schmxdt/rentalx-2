3a55878b202ed7e94896e78951f945e72bfbf03d
Schmxdt <airtonschmidt2018@gmail.com>
2023-02-01 11:19:17 -0300

    Insert into Repositories the UPDATE

--- src/modules/owner/infra/typeorm/repositories/OwnerRepository.ts
+++ src/modules/owner/infra/typeorm/repositories/OwnerRepository.ts
@@ -5,31 +5,73 @@ import { Owner } from "../entities/Owner";
 import { IOwnerDTO } from "@modules/owner/dtos/IOwnerDTO";
 import { IOwnerRepository } from "@modules/owner/repositories/IOwnerRepository";
 
+
+import { HttpResponse, notFound, ok, serverError } from "@shared/helpers/http";
+
+
 class OwnerRepository implements IOwnerRepository {
   private repository: Repository<Owner>;
 
   constructor() {
     this.repository = getRepository(Owner);
   }
+
+  // update
+  async update({
+    id,
+    name,
+    celular,
+    inativo
+  }: IOwnerDTO): Promise<HttpResponse> {
+    const owner = await this.repository.findOne(id)
+
+    if (!owner) {
+      return notFound()
+    }
+
+    const newowner = this.repository.create({
+      id,
+      name,
+      celular,
+      inativo
+    })
+
+    try {
+      await this.repository.save(newowner)
+
+      return ok(newowner)
+    } catch (err) {
+      return serverError(err)
+    }
+  }
+
+  // delete 
+
   async delete(id: string): Promise<any> {
 
     return await this.repository.delete(id);
   }
 
+  //create
+
   async create({
     name,
     inativo,
     celular
-  }: IOwnerDTO): Promise<Owner> {
-    const owner = this.repository.create({
+  }: IOwnerDTO): Promise<HttpResponse> {
+    const newowner = this.repository.create({
       name,
       inativo,
       celular
     });
 
-    await this.repository.save(owner);
+    try {
+      await this.repository.save(newowner)
 
-    return owner;
+      return ok(newowner)
+    } catch (err) {
+      return serverError(err)
+    }
   }
 }
 export { OwnerRepository };
--- src/modules/owner/repositories/IOwnerRepository.ts
+++ src/modules/owner/repositories/IOwnerRepository.ts
@@ -1,14 +1,18 @@
+import { HttpResponse } from "@shared/helpers/http";
 import { IOwnerDTO } from "../dtos/IOwnerDTO";
-import { Owner } from "../infra/typeorm/entities/Owner";
+
 
 
 interface IOwnerRepository {
 
   // create
-  create(data: IOwnerDTO): Promise<Owner>;
+  create(data: IOwnerDTO): Promise<HttpResponse>;
 
   //delete
-  delete(id: string): Promise<Owner>;
+  delete(id: string): Promise<HttpResponse>;
+
+  // update
+  update(data: IOwnerDTO): Promise<HttpResponse>
 }
 
 export { IOwnerRepository };
