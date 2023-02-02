f7e43359c73e8ac27b57771b90e0c6a8e03a13be
Schmxdt <airtonschmidt2018@gmail.com>
2023-02-02 11:00:00 -0300

    Insert into the List Route

--- src/shared/infra/http/routes/owners.routes.ts
+++ src/shared/infra/http/routes/owners.routes.ts
@@ -2,6 +2,7 @@ import { CreateOwnerController } from "@modules/owner/useCases/createOwner/Creat
 import { DeleteOwnerController } from "@modules/owner/useCases/deleteOwner/DeleteOwnerController";
 import { UpdateOwnerController } from "@modules/owner/useCases/updateOwner/UpdateOwnerController";
 import { ReadOwnerController } from "@modules/owner/useCases/readOwner/ReadOwnerController";
+import { ListOwnerController } from "@modules/owner/useCases/listOwner/ListOwnerController";
 
 import { Router } from "express";
 
@@ -11,5 +12,6 @@ ownersRoutes.post('/', new CreateOwnerController().handle)
 ownersRoutes.delete('/:id', new DeleteOwnerController().handle)
 ownersRoutes.put('/:id', new UpdateOwnerController().handle)
 ownersRoutes.get('/:id', new ReadOwnerController().handle)
+ownersRoutes.get('/', new ListOwnerController().handle)
 
 export { ownersRoutes }
